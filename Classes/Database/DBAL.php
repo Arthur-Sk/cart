3ade156ac14df86e43df6d3c513b4cf46c1a161c
Arthur <32418088+ApTypp@users.noreply.github.com>
2018-02-24 01:12:31 +0200

    corrections

--- Classes/Database/DBAL.php
+++ Classes/Database/DBAL.php
@@ -52,53 +52,6 @@ class DBAL extends Database{
         return true;
     }
 
-    public function createUser ($object, array $parameters) {
-        // Checks for unique username
-        if (!$this->isFieldUnique($object, array('username'=>$parameters['username']))){
-            $this->HandleError('Account with this username already exist');
-            return false;
-        }
-        // Hash the password
-        $salt = Database::getSalt();
-        $parameters['password'] = crypt($parameters['password'],'$2y$07$'.$salt);
-        // Saves to database
-        try {
-            $this->save($object, $parameters);
-        } catch (\PDOException $e) {
-            echo 'Smth else wrong: ', $e->getMessage(), "\n";
-            return false;
-        }
-        return true;
-    }
-
-
-    public function CheckLoginDB($object, array $parameters){
-        $result = $this->selectBy($object,['username' => $parameters['username']]);
-        if ($acc = $result->fetch()){
-            if (hash_equals($acc['password'], crypt($parameters['password'], $acc['password']))) {
-                //Starts season
-                session_unset();
-                session_destroy();
-                session_start();
-
-                $_SESSION['username'] = $acc['username'];
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public function login($object, array $parameters){
-
-        // Checks username and pass in DB
-        if (!$this->CheckLoginDB($object, $parameters)){
-            $this->HandleError('Error logging in. The username or password does not match');
-            return false;
-        }
-        return true;
-
-    }
-
     /**
      * @return string
      */
